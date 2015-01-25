3190c75cbf208573d233e35b5fc70388c987d932
nelsonic <contact.nelsonic@gmail.com>
2015-01-25 23:11:35 +0000

    almost there. loadFixture method complete. https://github.com/nelsonic/esta/issues/11

--- test/search_setup.js
+++ test/search_setup.js
@@ -5,39 +5,51 @@ var fixtures = __dirname + '/fixtures';
 // start timer
 var start = new Date().getTime();
 var count = 0;
+var filecount = 0
 
-fs.readdir(fixtures, function(err, files) {
-  var filecount = files.length;
-  // console.log(' >> '+filecount);
-  for(var i in files) {
-    var file = fixtures +'/' +files[i];
-    // console.log(file);
-    fs.readFile(file, function(err, data) {
-      if(err){
-        return console.log('Error obtaining data.', err);
+function loadFixtures(callback){
+  fs.readdir(fixtures, function(err, files) {
+    filecount = files.length;
+    // console.log(' >> '+filecount);
+    for(var i in files) {
+      var file = fixtures +'/' +files[i];
+      processFile(file, callback);
+    }
+  });
+}
+
+
+function processFile(filename, callback) {
+  fs.readFile(filename, function(err, data) {
+    if(err){
+      return console.log('Error obtaining data.', err);
+    }
+    var str = data.toString();
+    try {
+      JSON.parse(str);
+    } catch (e) {
+      return false;
+    }
+    var record = JSON.parse(str);
+    ES.CREATE(record, function(res) {
+      // if(res.created == true){
+      count++;
+      // }
+      // console.log(count + ' of ' + filecount);
+      if(filecount === count+1){
+        var end  = new Date().getTime();
+        var elapsed = (end-start)/1000;
+        var writerate = Math.round((filecount / elapsed), -1);
+        var log = chalk.black.bgGreen.bold('PERFORMANCE >>>> ') + chalk.cyan(' ' + filecount) + chalk.green(' Records Inserted in ');
+        log = log + chalk.cyan(elapsed) + chalk.green(' seconds ') + chalk.yellow(' ~ ');
+        log = log + chalk.cyan(writerate) + chalk.green(' records per second');
+        console.log(log);
+        return callback();
       }
-      var str = data.toString();
-      try {
-        JSON.parse(str);
-      } catch (e) {
-        return false;
-      }
-      var record = JSON.parse(str);
-      ES.CREATE(record, function(res) {
-        // if(res.created == true){
-          count++;
-        // }
-        // console.log(count + ' of ' + filecount);
-        if(filecount === count+1){
-          var end  = new Date().getTime();
-          var elapsed = (end-start)/1000;
-          var writerate = Math.round((filecount / elapsed), -1);
-          var log = chalk.black.bgGreen.bold('PERFORMANCE >>>> ') + chalk.cyan(' ' + filecount) + chalk.green(' Records Inserted in ');
-          log = log + chalk.cyan(elapsed) + chalk.green(' seconds ') + chalk.yellow(' ~ ');
-          log = log + chalk.cyan(writerate) + chalk.green(' records per second');
-          console.log(log)
-        }
-      });
     });
-  }
+  });
+}
+
+loadFixtures(function() {
+  console.log(chalk.black.bgGreen.bold(' - - - - - - - now we can search...'));
 });
