735ec9e1017db03d5c5f3b8d8bcbec190aaf7639
nelsonic <contact.nelsonic@gmail.com>
2015-01-26 20:19:29 +0000

    log connection stats to console for info/debug

--- test/connection.js
+++ test/connection.js
@@ -2,9 +2,10 @@ var test  = require('tape');
 var chalk = require('chalk');
 var C = require('../lib/connection.js');
 
-test(chalk.cyan('CONNECT to ES on localhost:9200'), function (t) {
+test(chalk.cyan('CONNECT to ES on 127.0.0.1:9200'), function (t) {
   C.checkConnection(function (err, res) {
     t.equal(err, null, chalk.green("✓ No Errors Connecting"));
+    console.log(res);
     t.equal(res.status, 200, chalk.green("✓ Status 200 - OK"));
     t.end();
   });
