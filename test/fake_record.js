16f1e409d61468949d53083c8d14278f0e34faa3
nelsonic <contact.nelsonic@gmail.com>
2015-02-04 21:21:51 +0000

    removes faker.js https://github.com/nelsonic/esta/issues/50

--- test/fake_record.js
+++ test/fake_record.js
@@ -1,10 +1,8 @@
-var faker = require('faker');
-
 module.exports = function() { // fake record
   return {
     type: 'tweet',
     index: 'twitter',
     id: Math.floor(Math.random() * (1000000000000)),
-    message: faker.hacker.phrase()
+    message: 'My Random Message '+this.id
   }
 }
--- test/options.js
+++ test/options.js
@@ -1,17 +1,10 @@
 var test  = require('tape');
 var chalk = require('chalk');
-var faker = require('faker');
+var record = require('./fake_record.js')(); // fake record
 
 var OPTIONS = require('../lib/options.js');
 
 test(chalk.cyan('Post OPTIONS object'), function (t) {
-  var record = {
-    type: 'tweet',
-    index: 'twitter',
-    id: Math.floor(Math.random() * (1000000)),
-    message: faker.hacker.phrase()
-  }
-
   var opts = OPTIONS(record, 'DELETE');
   t.equal(opts.method, 'DELETE', chalk.green("✓ Options Works"));
   t.end();
