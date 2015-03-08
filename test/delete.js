8166ccd08013e8ba7689325ddae8108060da2864
nelsonic <contact.nelsonic@gmail.com>
2015-03-08 15:18:08 +0000

    adds test to attempt to delete a non-existent record

--- test/delete.js
+++ test/delete.js
@@ -1,10 +1,18 @@
 var test  = require('tape');
 var chalk = require('chalk');
 var record = require('./fake_record.js')();
+var DELETE = require('../lib/delete.js');
+
+test(chalk.cyan('DELETE a Non-Existant Record'), function (t) {
+  DELETE(record, function(response) {
+      console.log(response);
+      t.equal(response.found, false, chalk.green("✓ Record not found (no delete performed)"));
+      t.end();
+  });
+});
 
 var CREATE = require('../lib/create.js');
 var READ = require('../lib/read.js');
-var DELETE = require('../lib/delete.js');
 var FS     = require('../lib/fs.js');
 
 test(chalk.cyan('DELETE a Record'), function (t) {
@@ -16,10 +24,12 @@ test(chalk.cyan('DELETE a Record'), function (t) {
   }
   CREATE(record, function(res) {
     DELETE(rec, function(res3) {
+        // console.log(res3);
         t.equal(res3.found, true, chalk.green("✓ Record Existed - So DELETE it!"));
+        t.equal(res3.deleted, true, chalk.green("✓ Record DELETEd"));
       // attempt to read record - it should fail
       READ(rec, function(res4){
-        t.equal(res4.found, false, chalk.green("✓ Record Deleted"));
+        t.equal(res4.found, false, chalk.green("✓ Record Deletion CONFIRMED"));
         // console.log(' - - - ')
         // console.log(rec);
         // console.log(' - - - ')
