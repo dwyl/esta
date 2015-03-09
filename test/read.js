0d7edd0fd1d1d0d1a6f14a0c1c75aa1923c3eb39
nelsonic <contact.nelsonic@gmail.com>
2015-03-09 00:43:53 +0000

    minor update to read.js

--- test/read.js
+++ test/read.js
@@ -1,7 +1,6 @@
 var test  = require('tape');
 var chalk = require('chalk');
 var RECORD = require('./fake_record.js');
-
 var CREATE = require('../lib/create.js');
 var READ = require('../lib/read.js');
 
@@ -16,7 +15,6 @@ test(chalk.cyan('READ a record'), function (t) {
   CREATE(record, function(res) {
     t.equal(res.created, true, chalk.green("✓ Record Created " +rec.id));
     READ(rec, function (res2) {
-      // console.log(res2)
       t.equal(res2._source.message, rec.message, chalk.green("✓ Record fetched " + res2._id + " " + res2._source.message));
       t.end();
     });
@@ -26,7 +24,6 @@ test(chalk.cyan('READ a record'), function (t) {
 test(chalk.cyan('READ a record that does not exist (expect found === false)'), function (t) {
   var record = RECORD();
   READ(record, function (res2) {
-    // console.log(res2)
     t.equal(res2.found, false, chalk.green("✓ Record " + res2._id + " Not Found (as expected)"));
     t.end();
   });
