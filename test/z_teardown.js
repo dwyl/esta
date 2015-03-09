f74c7757c076d05e1f6ac0772b0ea27851daae39
nelsonic <contact.nelsonic@gmail.com>
2015-03-09 00:39:16 +0000

    tidy up test/z_teardown.js script

--- test/z_teardown.js
+++ test/z_teardown.js
@@ -14,35 +14,6 @@ var record = {
   message: "what evs"
 }
 
-// // create another few files to exercise :
-// test(chalk.cyan('Create dummy records to exercise ') + chalk.red('deleteDataDir ') + chalk.cyan('method )'), function (t) {
-//   record.id = 12345;
-//   FS.saveFile(record, function(){
-//     // console.log(' - - - - - - - ');
-//     // console.log(record.id);
-//     record.id = 65432;
-//     FS.saveFile(record, function(){
-//       // console.log(' - - - - - - - ');
-//       // console.log(record.id);
-//       FS.fileExists(record, function (exists) {
-//         t.equal(exists, true, chalk.green("✓ ") + chalk.red('record created'));
-//         t.end();
-//       });
-//
-//     });
-//   });
-// });
-
-// test(chalk.cyan('TIDY UP TIME ( delete all files in ') + chalk.red('_data ') + chalk.cyan('directory )'), function (t) {
-//   D.deleteDataDir(function (err, deleted) {
-//     t.equal(deleted, true, chalk.green("✓ ") + chalk.red('_data DELETED!'));
-//     FS.dataDirExists(function (err, exists) {
-//       t.equal(exists, false, chalk.green("✓ ") + chalk.red('_data ') + chalk.green("dir should no longer exist!"));
-//       t.end();
-//     });
-//   });
-// });
-
 // NOT Exposing this method in the module because "DROP DATABASE" is too much power!
 var DROP  = require('../lib/z_teardown.js');
 var STATS = require('../lib/stats.js');
