10e6c784c8a4efa8e6927660ca6d8eb6c0d91679
Nelson <contact.nelsonic@gmail.com>
2015-08-06 17:33:14 +0100

    adds rudimentary check for *presence* of http_request options https://github.com/dwyl/esta/issues/79

--- test/request.test.js
+++ test/request.test.js
@@ -1,8 +1,19 @@
 var test     = require('tape');
 var REQUEST  = require('../lib/http_request');
 var OPTIONS  = require('../lib/options')
+var optsmsg  = "requires valid http request options";
 var cberrmsg = "please supply a callback"
 
+test('Attepmt to invoke REQUEST WITHOUT http request options', function(t) {
+  try {
+    // var options = OPTIONS({}, 'GET');
+    var result  = REQUEST();
+  } catch (error){
+    console.log(error);
+    t.ok(error.indexOf(optsmsg) > -1, "*Wanted Error* Got "+error + " (as expected!)");
+    t.end();
+  }
+})
 
 test('Attepmt to invoke REQUEST WITHOUT VALID callback funciton', function(t) {
   try {
