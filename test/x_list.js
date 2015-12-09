cfb0ab151facf8a323f2fc261f285cf013eb7fbf
nelsonic <contact.nelsonic+github@gmail.com>
2015-12-09 22:51:19 +0000

    adds test for list _all index/types

--- test/x_list.js
+++ test/x_list.js
@@ -18,3 +18,15 @@ test(chalk.cyan("LIST all _ids of records in index"), function(t) {
       t.end();
     });
 });
+
+test(chalk.cyan("LIST all _ids of records in _all indecies"), function(t) {
+    var query = {};
+    LIST(query, function(res){
+      console.log(' - - - - - - - - - - - - - - - - - - - - - res:');
+      // console.log(JSON.stringify(res, null, 2));
+      console.log(res.hits.hits.length)
+      console.log(' - - - - - - - - - - - - - - - - - - - - - - - - ');
+      t.ok(res.hits.hits.length > 9, "List returned "+res.hits.hits.length + " hits.")
+      t.end();
+    });
+});
